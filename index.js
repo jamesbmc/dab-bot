5d8a78b7b3d2d2290924edd1158b09de7548c75c
jamesbmc <jbmc@uw.edu>
2019-05-01 16:54:47 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import * as secrets from './auth.json'
+
+const token = secrets.token
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('message', msg => {
+    msg.react(msg.guild.emojis.get('535958853519540233'))
+    .catch(console.error);
+})
+
+client.login(token)
